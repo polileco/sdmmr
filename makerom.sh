3c752f0571976f4d2741be05e3b6a5ab29580469
polileco <leandro.polimeno@gmail.com>
2023-06-14 11:47:24 -0300

    Organization of files and directories

--- makerom.sh
+++ makerom.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-VERSION=1.0.5
+if [[ -z $VERSION ]]; then
+ echo "VERSION variable not set."
+ echo "Ex.: export VERSION=1.0.5 ./makerom"
+ exit 1
+fi
 
-./mknexrom Nextor-2.1.1.base.dat sdmmr_v${VERSION}.rom \
+if [[ -f ./firmware/sdmmr_v${VERSION}.rom ]]; then
+ echo "./firmware/sdmmr_v${VERSION}.rom already exist. Delete it before make a new one."
+ exit 1
+fi
+ 
+./mknexrom ./nextor/Nextor-2.1.1.base.dat ./firmware/sdmmr_v${VERSION}.rom \
   /d:device.bin \
-  /m:Mapper-ASCII8.noheader.bin \
+  /m:./nextor/Mapper-ASCII8.noheader.bin \
   /e:custom_code.bin \
   /8:6000
 
