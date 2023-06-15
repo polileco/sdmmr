db877dbbe80804e1fb9ae6f8fa721ea4f3a0d56e
polileco <leandro.polimeno@gmail.com>
2023-06-15 18:19:01 -0300

    Enhanced script

--- makerom.sh
+++ makerom.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [[ -z $VERSION ]]; then
- echo "VERSION variable not set."
- echo "Ex.: export VERSION=1.0.5 ./makerom"
+if [[ -z $VERSION || -z $NEXTOR ]]; then
+ echo "Variables not set."
+ echo "Ex.: export VERSION=1.0.5 NEXTOR=2.1.1 && ./makerom.sh"
  exit 1
 fi
 
@@ -11,7 +11,7 @@ if [[ -f ./firmware/sdmmr_v${VERSION}.rom ]]; then
  exit 1
 fi
  
-./mknexrom ./nextor/Nextor-2.1.1.base.dat ./firmware/sdmmr_v${VERSION}.rom \
+./mknexrom ./nextor/Nextor-${NEXTOR}.base.dat ./firmware/sdmmr_v${VERSION}.rom \
   /d:device.bin \
   /m:./nextor/Mapper-ASCII8.noheader.bin \
   /e:custom_code.bin \
