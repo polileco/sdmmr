4f323a4a09e3c8aab3af388b1ca731939d49a56d
polileco <leandro.polimeno@gmail.com>
2023-12-19 15:43:40 -0300

    Update firmware and kernel to Nextor 2.1.2

--- makerom.sh
+++ makerom.sh
@@ -2,16 +2,16 @@
 
 if [[ -z $VERSION || -z $NEXTOR ]]; then
  echo "Variables not set."
- echo "Ex.: export VERSION=1.0.5 NEXTOR=2.1.1 && ./makerom.sh"
+ echo "Ex.: export VERSION=1.0.6 NEXTOR=2.1.2 && ./makerom.sh"
  exit 1
 fi
 
-if [[ -f ./firmware/sdmmr_v${VERSION}.rom ]]; then
- echo "./firmware/sdmmr_v${VERSION}.rom already exist. Delete it before make a new one."
+if [[ -f ./firmware/sdmmr${VERSION//./}.rom ]]; then
+ echo "./firmware/sdmmr${VERSION//./}.rom already exist. Delete it before make a new one."
  exit 1
 fi
  
-./mknexrom ./nextor/Nextor-${NEXTOR}.base.dat ./firmware/sdmmr_v${VERSION}.rom \
+./mknexrom ./nextor/Nextor-${NEXTOR}.base.dat ./firmware/sdmmr${VERSION//./}.rom \
   /d:device.bin \
   /m:./nextor/Mapper-ASCII8.noheader.bin \
   /e:custom_code.bin \
